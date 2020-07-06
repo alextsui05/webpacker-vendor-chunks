e6cd1b9e7e803a6974b9e890804fc3fba179b45e
Alex Tsui <alextsui@pm.me>
2020-07-06 17:51:05 +0000

    :ok_hand: Configure webpacker to split vendor chunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,30 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.config.merge({
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        // @see https://hackernoon.com/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+          priority: 10,
+        },
+      }
+    }
+  }
+})
 
 module.exports = environment
